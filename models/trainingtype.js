3d38b2cac835e08fe381688cbff238d3b8226eb2
powsitu <sendtopow@gmail.com>
2021-05-04 15:32:23 +0200

    added relation between trainingType and image tables

--- models/trainingtype.js
+++ models/trainingtype.js
@@ -11,6 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      trainingType.hasOne(models.image);
     }
   };
   trainingType.init({
