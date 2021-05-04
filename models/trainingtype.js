a0ddf66ac14aeaab6e9a2aada134d7a714fe3554
powsitu <sendtopow@gmail.com>
2021-05-04 11:22:48 +0200

    added trainingType model with seeds

---
+++ models/trainingtype.js
@@ -0,0 +1,25 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class trainingType extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  trainingType.init({
+    name: DataTypes.STRING,
+    description: DataTypes.STRING,
+    intensity: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'trainingType',
+  });
+  return trainingType;
+};
